090351f625def0a300656cb1fd960b80dc5613eb
Halil Ibrahim Ozturk <Hiboztrk@gmail.com>
2026-08-06 00:47:28 +0200

    test: import vitest globals explicitly

--- components/Sidebar.test.tsx
+++ components/Sidebar.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
 import { Sidebar } from './Sidebar';
 import { ModuleType, ViewMode } from '../types';
 
