506bfb67305abaaa86c67f489004e1aca0eb2e31
Halil Ibrahim Ozturk <Hiboztrk@gmail.com>
2026-08-06 01:05:35 +0200

    feat: personalize Capacitor app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.inmersion.mathbiotech',
-  appName: 'Math Biotech',
+  appName: 'Inmerson Lab',
   webDir: 'dist'
 };
 
